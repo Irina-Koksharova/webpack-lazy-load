7bbd94a28da4924e24d5f174afb67a0b41155666
Irina-Koksharova <biodgerelo@ukr.net>
2020-10-16 21:39:27 +0300

    Добавляет разметку и стили галереи

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './styles.css';
+import { menuMarkup, menuList, checkbox } from './js/markup';
+import { Theme, onCheckboxClick, favoriteTheme } from './js/toggle';
+
+menuList.insertAdjacentHTML('afterbegin', menuMarkup);
+
+document.body.classList.add(Theme.LIGHT);
+checkbox.addEventListener('change', onCheckboxClick);
+
+favoriteTheme();
